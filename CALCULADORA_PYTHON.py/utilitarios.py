3f19ccb33d052d950aae987af502f4c81070eea4
Nicole Santos <santosnicole059@gmail.com>
2026-02-04 15:00:18 -0300

    Update utilitarios.py

--- CALCULADORA_PYTHON.py/utilitarios.py
+++ CALCULADORA_PYTHON.py/utilitarios.py
@@ -2,7 +2,6 @@ def validar_entrada(valor):
 
         num = float(valor)
 
-        """Verifica de o float é um int, se sim = tira o decimal, senão, mantém float"""
         if num.is_integer():
             return int(num)
         
